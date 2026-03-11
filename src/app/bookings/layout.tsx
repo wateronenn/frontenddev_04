c1dfdcadb9ee3d03602820a7ddffdecb6c190faa
wateronenn <anamneung@gmail.com>
2026-03-12 00:09:18 +0700

    finish assignment4

---
+++ src/app/bookings/layout.tsx
@@ -0,0 +1,7 @@
+export default function BookingsLayout({children}:{children:React.ReactNode}){
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
---
+++ src/app/bookings/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Bookings(){
+    return(
+        <main style={{marginTop:"50px", textAlign:"center",fontSize:"40px"}}>
+            <div className="text-white"> Venue Bookings</div>
+        </main>
+    )
+}
